3cac2f0347f9eed79ccebd7c104f1e5430edfdd8
Ramadevi09 <51950204+Ramadevi09@users.noreply.github.com>
2019-07-19 12:40:08 +0530

    Update MovieConfig.java

--- src/main/java/com/stackroute/domain/MovieConfig.java
+++ src/main/java/com/stackroute/domain/MovieConfig.java
@@ -6,13 +6,16 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 @Configuration
+//property files location
 @PropertySource("classpath:actorvalues.properties")
 public class MovieConfig {
+    //creating actor object 
     @Bean
     public Actor actor(){
         Actor actor=new Actor();
         return actor;
     }
+    //injecting actor object in movie
     @Bean
     public Moive moive(){
         Moive moive=new Moive();
