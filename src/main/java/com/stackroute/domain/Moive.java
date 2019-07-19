4fafae9c81000462d6367768eb951627b226c35b
Ramadevi09 <51950204+Ramadevi09@users.noreply.github.com>
2019-07-19 12:37:13 +0530

    Update Moive.java

--- src/main/java/com/stackroute/domain/Moive.java
+++ src/main/java/com/stackroute/domain/Moive.java
@@ -15,12 +15,6 @@ public class Moive {
 
     private Actor actor;
 
-
-
-   /* public Moive(Actor actor) {
-        this.actor = actor;
-    }*/
-
     public void setActor(Actor actor) {
         this.actor = actor;
     }
