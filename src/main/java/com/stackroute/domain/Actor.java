84f6c13bc24bdb34dac3ffc8378e763f91d6286d
Ramadevi09 <51950204+Ramadevi09@users.noreply.github.com>
2019-07-21 16:41:04 +0530

    Update Actor.java

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Actor {
+    //injecting actor values using @value annotation
 @Value("${actor.Name}")
     private String name;
 @Value("${actor.Gender}")
